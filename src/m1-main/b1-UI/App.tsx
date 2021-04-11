280c04f1f2232d6f850c9a701c3b5f56a1282937
AntonDzyadevich <antonbilder86@gmail.com>
2021-04-11 23:22:32 +0300

    first commit

---
+++ src/m1-main/b1-UI/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+        {/*hr,prov*/}
+
+          <>
+
+              {/*<Main/>*/}
+
+          </>
+    </div>
+  );
+}
+
+export default App;
